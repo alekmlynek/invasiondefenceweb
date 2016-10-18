b2b077e8d88c12b5d9ac4c4c3b20b75f506a0190
Alek Mlynek <alek@alek.local>
2016-10-18 00:03:05 -0600

    Invasion defence website source

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+// Cycle after images load
+$(window).load(function()
+{
+  $("#screenshots").cycle({
+    speed: 300,
+    timeout: 4000
+  });
+});
+
+
+
+// Parallax
+(function() {
+  window.addEventListener('scroll', function(event) {
+    var depth, i, layer, layers, len, movement, topDistance, translate3d;
+    topDistance = this.pageYOffset;
+    layers = document.querySelectorAll("[data-type='parallax']");
+    for (i = 0, len = layers.length; i < len; i++) {
+      layer = layers[i];
+      depth = layer.getAttribute('data-depth');
+      movement = -(topDistance * depth);
+      translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+      layer.style['-webkit-transform'] = translate3d;
+      layer.style['-moz-transform'] = translate3d;
+      layer.style['-ms-transform'] = translate3d;
+      layer.style['-o-transform'] = translate3d;
+      layer.style.transform = translate3d;
+    }
+  });
+
+}).call(this);
+
+
